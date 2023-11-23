6594bc3e2da3879c11f21d8261942f51c2d4b3b5
Documenter.jl <documenter@juliadocs.github.io>
2023-11-23 00:44:53 +0000

    build based on b5c78c4

---
+++ dev/script_examples/ExampleScript.jl
@@ -0,0 +1,38 @@
+#=
+# Script example
+=#
+
+#
+# Import stuff
+#
+using Pkg
+using ExampleJuggler
+import CairoMakie
+using Test
+#
+# Run stuff
+#
+
+x, fx = mock_x()
+maxfx = maximum(fx)
+
+#
+# Plot of the  result
+# ![](ExampleScript.svg)
+#
+# We can test when the script runs
+@test isapprox(maxfx, 1.0; rtol = 1.0e-3)
+
+# If we don't want the tests in the script we 
+# can test in the runtests function
+function runtests(; kwargs...)
+    println("runtests from  testscript")
+    @test isapprox(maxfx, 1.0; rtol = 1.0e-3)
+end
+
+# Here we generate plots
+function generateplots(dir; kwargs...)
+    CairoMakie.activate!(; type = "svg", visible = false)
+    p = CairoMakie.lines(x, fx)
+    CairoMakie.save(joinpath(dir, "ExampleScript.svg"), p)
+end
